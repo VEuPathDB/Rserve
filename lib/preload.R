f402d7ae42225ab2b0f43840651918ed3a0e32c0
Danielle Callan <dcallan@upenn.edu>
2021-09-10 13:18:12 -0400

    freadWithLogging helper

--- lib/preload.R
+++ lib/preload.R
@@ -7,4 +7,5 @@ library(DESeq2)
 ## NOTE: attach our in house packages last
 library(plot.data)
 files.sources <- list.files('opt/rserve/lib/R/', full.names=TRUE, recursive=TRUE)
+message('\n', Sys.time(), 'Preloading functions from the following files: ', files.sources)
 invisible(sapply(files.sources, source))
