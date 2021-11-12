0bff8e11f3120470e6eecbf3b8e0d78c816f7139
Danielle Callan <53306535+d-callan@users.noreply.github.com>
2021-11-12 13:10:27 -0500

    update attached libraries

--- lib/preload.R
+++ lib/preload.R
@@ -1,16 +1,11 @@
 library(data.table)
-library(ape)
-library(vegan)
-library(phytools)
-library(rbiom)
-library(DESeq2)
 library(Rcpp)
 ## NOTE: attach our in house packages last
-library(plot.data)
 library(veupathUtils)
+library(plot.data)
 library(microbiomeComputations)
 
 files.sources <- list.files('opt/rserve/lib/R/', full.names=TRUE, recursive=TRUE)
-message('\n', Sys.time(), 'Preloading functions from the following files: ', files.sources)
+message('\n', Sys.time(), ' Preloading functions from the following files: ', files.sources)
 invisible(sapply(files.sources, source))
 
